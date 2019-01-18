c0ad76b734165e3a8f3f40684d0845c0e81e2130
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-01-17 18:46:08 -0800

    added profile view

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,4 +8,10 @@ module.exports = function (app) {
             pageTitle: "Scraper Scraper App"
         });
     });
+
+    app.get("/profile", function (req, res) {
+        res.render("profile", {
+            pageTitle: "Profile Page"
+        });
+    });
 }
