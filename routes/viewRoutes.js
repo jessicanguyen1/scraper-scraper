502be60d7aff62955d5621a71c66bce484c8fd55
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-01-20 20:17:50 -0800

    fixing css

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,34 +1,38 @@
 const path = require("path");
 const cheerio = require("cheerio");
 const request = require("request");
-const results = [];
-
+var results = [];
+var newResults;
 
 module.exports = function (app) {
 
     // Root Route
     app.get("/", function (req, res) {
         res.render("index", {
-            pageTitle: "Scraper Scraper App"
+            pageTitle: "Scraper Scraper App",
+            results: results
         });
     });
 
     app.get("/saved-articles", function (req, res) {
         res.render("saved-articles", {
-            pageTitle: "Saved Articles"
+            pageTitle: "Saved Articles",
+            results: results
         });
     });
 
     // API routes
     app.get("/api/get-articles", function (req, res) {
+
+
         // Use library to scrape articles 
         request('https://www.marinij.com/news/', function (error, response, body) {
             if (!error && response.statusCode == 200) {
 
                 let $ = cheerio.load(body);
+
+                newResults = [];
                 //console.log($('h5.entry-title').children())
-
-
 
                 $('h5.entry-title').each(function (i, element) {
                     // Save the text of the element in a "title" variable
@@ -38,17 +42,23 @@ module.exports = function (app) {
                     // console.log(title)
 
                     // Save these results in an object that we'll push into the results array we defined earlier
-                    results.push({
+                    newResults.push({
                         title: title,
                         link: link
                     });
-                    console.log(results);
+
+                    results = newResults;
                     // let a = $(this).prev();
                     // console.log(a.text());
+
                 });
+                // return res.redirect("index", {
+                //     results: results
+                // });
             }
         });
-        res.send(results);
+        // console.log(results);
+        // res.redirect("/");
     });
 
 }
